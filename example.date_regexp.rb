883395d38268a16e2c1112746362d8864b819460
JohnPSYMQEPDFQ <john_privara@yahoo.com>
2020-09-29 12:53:41 -0700

    Update example.date_regexp.rb

--- example.date_regexp.rb
+++ example.date_regexp.rb
@@ -1,20 +1,20 @@
 require 'module.ArchivesSpace.Konstants.rb'
 
-input_record = "leading text, 1950-1959, November 21, 1860, Dec 15, 1931, 1940 trailing text"
+input_record = "leading text, 1950-1959, November 21, 1860, Dec 15, 1931, 1940"
 pp input_record
 
 date_A = []
 loop do
     regexp = %r{
-               \s*(?:,)?\s*(
+               \s*(?:,)?\s*(          
                              (#{K.month_RE}\s+#{K.day_RE},\s+)?#{K.year4_RE}\s*-\s*(#{K.month_RE}\s+#{K.day_RE},\s+)?#{K.year4_RE}
                             |(#{K.month_RE}\s+#{K.day_RE},\s+)?#{K.year4_RE}
                            )
-               }x
+               (?:\s+|$)}x
     match_O = input_record.match( regexp )  # date pattern match
     if ( match_O ) then
         date_A << match_O[ 1 ]
-        input_record.sub!( regexp, "" )
+        input_record.sub!( regexp, " " )
     else
         break
     end
