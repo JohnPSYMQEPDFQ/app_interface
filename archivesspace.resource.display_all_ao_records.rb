9760e0c097c754767edf2088e8572cbb05da56a0
John P <john_privara@yahoo.com>
2023-06-16 18:38:14 -0700

    Mindless tinkering...

--- archivesspace.resource.display_all_ao_records.rb
+++ archivesspace.resource.display_all_ao_records.rb
@@ -18,15 +18,9 @@ require 'json'
 # require 'pp'
 require 'optparse'
 
-# require 'class.Array.extend.rb'
-# require 'class.String.extend.rb'
-# require 'class.Hash.extend.rb'
-# require 'module.SE.rb'
 require 'class.Archivesspace.rb'
-# require 'class.ArchivesSpace.http_calls.rb'
 require 'class.Archivesspace.ArchivalObject.rb'
 require 'class.Archivesspace.Repository.rb'
-#require 'class.Archivesspace.TopContainer.rb'
 require 'class.Archivesspace.Resource.rb'
 
 
